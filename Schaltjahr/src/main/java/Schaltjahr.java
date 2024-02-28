696d6b7234566fca1fafdb5f966983f3e6e610fa
Lukas Cotti <lukas.cotti@hftm.ch>
2024-02-28 17:44:14 +0100

    Erweiterung für Schaltjahre

--- Schaltjahr/src/main/java/Schaltjahr.java
+++ Schaltjahr/src/main/java/Schaltjahr.java
@@ -1,24 +1,17 @@
 import javax.swing.JOptionPane;
-
 public class Schaltjahr {
-
-
     public static void main(String[] args) {
-        
         String input = JOptionPane.showInputDialog("Bitte eine Jahreszahl eingeben:");       
         double Jahr = Integer.parseInt(input);
-if (input.endsWith("00")) {
-    System.out.println("Das ist kein Schaltjahr weil: Es ist ein Jahrhundert");
-}
-else {
-        if ((Jahr / 4) % 1 == 0) {
-            System.out.println((int)Jahr + " ist ein Schaltjahr!");
+
+        if ( input.endsWith("00") && (Jahr /100) % 1 != 0 && (Jahr /400) % 1 != 0 ) {
+            System.out.println("Das ist kein Schaltjahr weil: Es ist ein Jahrhundert");
+        } else {
+            if ( (Jahr / 4) % 1 == 0 ) {
+                System.out.println((int)Jahr + " ist ein Schaltjahr!");
+            } else {
+                System.out.println((int) Jahr + " ist kein Schaltjahr!");
+            }
         }
-        else {
-            System.out.println((int) Jahr + " ist kein Schaltjahr!");
-        }
-        
-        //System.out.println(Jahr);
-    }
-}   
+    }   
 }
